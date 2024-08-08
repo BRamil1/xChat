dbc2006edd96f44165d1bfbb77009a0a2e667c3a
Radomyr <qulowg@gmail.com>
2024-08-08 22:47:06 +0300

    - fixing bugs in nav - changed "author" to "about"

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,5 @@ from main import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.index, name='home'),
-    path('author/', views.author, name='author'),
+    path('about/', views.about, name='about'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,5 +5,5 @@ def index(request):
     return render(request, "main/index.html")
 
 
-def author(request):
-    return render(request, "main/author.html")
+def about(request):
+    return render(request, "main/about.html")
